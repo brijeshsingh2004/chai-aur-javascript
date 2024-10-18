d9d8912ad57419dc4bf5ac96d5e9ab86ec13e867
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-10-18 07:13:33 +0000

    conversion of datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+// let score = "24837587ttytryryr9";
+
+
+// let valueInNumber = Number(score);
+
+
+// console.log(typeof score);
+
+// console.log(valueInNumber);
+
+
+
+
+let isLoggedIn = 1;
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+
+console.log(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
