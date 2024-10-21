7770b8881499a58ad0e49bf6fb41d4507bb5ef3f
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-10-21 05:38:26 +0000

    passaway

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -4,3 +4,26 @@
 
 // console.log(`hello my name is ${name} and my midname is ${midName}`);
 
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+// const gameName = new String('Brijesh singh');
+
+
+
+// console.log(gameName.charAt(0));
+// console.log(gameName[0]);
+// console.log(gameName.indexOf('B'));
+
+
+
+
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
