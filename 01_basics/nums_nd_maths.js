ae94c328590518e46389c63276899fa730f51e2f
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-10-21 08:57:24 +0000

    maths nd numbers

---
+++ 01_basics/nums_nd_maths.js
@@ -0,0 +1,33 @@
+
+// const score = 535.46557;
+// const balance = new Number(500);
+
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));
+// console.log(score.toPrecision(10));
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const hundred = 1000000;
+
+
+console.log(hundred.toLocaleString());
+console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++++++++++++++++++MATH++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(math);
+console.log(math.abs(-4));
+console.log(math.round(4.6));
+console.log(math.ceil(4.3));
+console.log(math.floor(4.8));
+
+console.log(math.min(4,8));
+console.log(math.max(4,8));
