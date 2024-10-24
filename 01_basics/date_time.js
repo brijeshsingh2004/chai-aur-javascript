e052516360d4ed0fcc6ff002b13bb8ed1a65b33d
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-10-24 07:23:05 +0000

    basics completed

---
+++ 01_basics/date_time.js
@@ -0,0 +1,35 @@
+
+// DATES
+
+
+// let newDate = new Date("11   , 21, 2024");
+
+
+
+// console.log(newDate.toLocaleString());
+// console.log(newDate.toString());
+// console.log(newDate.toDateString());
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+// console.log(Math.floor(Date.now() / 1000))
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// let myDate = new Date();
+
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth());
+// console.log(Math.floor (myDate.getTime()/1000));
