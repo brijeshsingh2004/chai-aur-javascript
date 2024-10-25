33ff87f4a4b960ac7164cf5e50c452c946c964c7
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-10-25 08:33:07 +0000

    basics2

---
+++ 02_basics/array2.js
@@ -0,0 +1,62 @@
+// const marvel = ["hulk" , "thor" , "vision"];
+// const dc = ["batman" , "superman" , "flash"];
+
+// marvel.push(dc);
+
+
+
+
+// console.log(marvel)
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+// const marvel = ["hulk" , "thor" , "vision"];
+// const dc = ["batman" , "superman" , "flash"];
+
+
+// const newHero = marvel.concat(dc)
+
+
+// console.log(newHero)
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+// const marvel = ["hulk" , "thor" , "vision"];
+// const dc = ["batman" , "superman" , "flash"];
+
+
+// const newHero = marvel.concat(dc)
+
+
+// console.log(newHero)
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+const marvel = ["hulk" , "thor" , "vision"];
+const dc = ["batman" , "superman" , "flash"];
+
+
+const newHero = [...marvel , ...dc];
+
+
+console.log(newHero)
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---
+++ 02_basics/objects.js
@@ -0,0 +1,20 @@
+
+
+let mySym = Symbol("key1")
+
+
+
+const student = {
+
+name: "brijesh thakur",
+age : 20,
+[mySym] : "key1",
+email:"brijeshthakurscs123",
+
+}
+
+
+console.log(student [mySym]) ;
+// console.log(typeof [mySym]);
+
+conso
