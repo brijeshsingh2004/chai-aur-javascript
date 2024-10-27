95fa821e2afa9c78365d9efc1f3b872b9db666e8
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-10-27 07:56:51 +0000

    object in depth

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -1,20 +1,103 @@
 
 
-let mySym = Symbol("key1")
+// let mySym = Symbol("key1")
+
+
+
+// const student = {
+
+// name: "brijesh thakur",
+// age : 20,
+// [mySym] : "key1",
+// email:"brijeshthakurscs123",
+
+// }
+
+
+// console.log(student [mySym]) ;
+// console.log(typeof [mySym]);
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+// const student = {
+
+//     name: "brijesh thakur",
+//     age : 20,
+//     email:"brijeshthakurscs123",
+    
+//     }
+    
+
+
+//     student.age = 21
+    
+//     console.log(student) ;
+  
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+// const student = {
+
+//     name: "brijesh thakur",
+//     age : 19,
+//     email:"brijeshthakurscs123",
+    
+//     }
+    
+
+
+//     student.age = 20
+    
+// Object.freeze(student)   // you can't update anything after freezing the object
+
+// student.age = 21
+
+//     console.log(student) ;
+  
+
+
+
+
+
+
+
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 
 
 
 const student = {
 
-name: "brijesh thakur",
-age : 20,
-[mySym] : "key1",
-email:"brijeshthakurscs123",
+    name: "brijesh thakur",
+    age : 19,
+    email:"brijeshthakurscs123",
+    
+    }
+    
 
-}
 
+    student.age = 20
+    
+Object.freeze(student)
 
-console.log(student [mySym]) ;
-// console.log(typeof [mySym]);
+student.age = 21
 
-conso
+    console.log(student) ;
+  
