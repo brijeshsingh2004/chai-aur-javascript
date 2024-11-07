518be2429dfccc4ce7260743ab81a7cad0deaab1
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-11-07 08:42:48 +0000

    for in begin

---
+++ loops/map.js
@@ -0,0 +1,23 @@
+// MAP DOESN'T PRINT DUPLICATE VALUE
+
+
+// map = new Map();
+// map.set(`IN` , "INDIA");
+
+
+
+// // console.log(map);
+
+
+
+
+
+// PRINTING MAP IN FOR OF LOOP
+
+
+// for (const [element, value] of map) {
+
+//     console.log(element, "=>", value);
+
+
+// }
