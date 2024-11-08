a754a64e9cc60f9abcc21475526f00d9cb5abf18
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-11-08 11:37:01 +0000

    gonna start foreach

--- loops/for_in.js
+++ loops/for_in.js
@@ -1,12 +1,53 @@
-const object ={
+// const object ={
 
-    name:" my lob neha is ",
-}
+//     name:" my lob neha is ",
+// }
 
-for (const key in object) {
+// for (const key in object) {
    
        
-        console.log(`${key} of${object[key]}`);
+//         console.log(`${key} of${object[key]}`);
     
     
-}
+// }
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const object ={
+
+// chotu:"brijesh",
+
+
+
+// }
+
+
+// for (const key in object) {
+  
+//     console.log(key ,"=> " ,object[key]);
+    
+
+
+// }
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const array = ["brijesh" , "neha"];
+
+
+// for (const key in array) {
+    
+
+// console.log(key);
+
+
+// }
