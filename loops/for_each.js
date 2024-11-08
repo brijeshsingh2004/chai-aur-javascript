d70696598530f2a3fb423f87f9e53797bfb42124
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-11-08 12:03:38 +0000

    hemlo

---
+++ loops/for_each.js
@@ -0,0 +1,22 @@
+// const coding = ["java" , "javaScript" , "cpp"];
+
+// coding.forEach( (value) => {
+
+// console.log(value);
+
+
+// }  )
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+const coding = ["java" , "javaScript" , "cpp"];
+coding.forEach( function(val){
+
+console.log(val);
+
+
+})
