7baf8becdc91940d1202e2e9e9a639e6b93b0715
brijeshsingh2004 <brijeshsinghscs1234@gmail.com>
2024-11-09 07:23:52 +0000

    for each completed

---
+++ loops/filter.js
@@ -0,0 +1,28 @@
+// coding = [1 , 2,4,5,6,7,8,9,10];
+
+
+// const hello = coding.filter((num)  => 
+// {
+    
+//   return  num>1
+
+
+// }
+
+// )
+// console.log(hello);
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+coding = [1 , 2,4,5,6,7,8,9,10];
+
+
+const hello = coding.filter((num)  => num>1);
+console.log(hello);
+
+
+
--- loops/for_each.js
+++ loops/for_each.js
@@ -13,10 +13,90 @@
 
 
 
-const coding = ["java" , "javaScript" , "cpp"];
-coding.forEach( function(val){
+// const coding = ["java" , "javaScript" , "cpp"];
+// coding.forEach( function(val){
 
-console.log(val);
+// console.log(val);
 
 
-})
+// })
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// const coding = ["java" , "javaScript" , "cpp"];
+
+
+
+// function hello (item)  {
+
+// console.log(item);
+
+
+// }
+
+// coding.forEach(hello);
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// const coding = [
+
+//     {
+
+//         name:"neha"
+//     },
+//     {
+
+//         name:"brijesh"
+//     },
+
+
+
+// ];
+
+
+// coding.forEach( (item) =>{
+
+// console.log(item.name);
+
+
+// }   )
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+// coding = [1, 2, 4, 5, 6, 7, 8, 9, 10];
+
+
+// // const hello = [];
+
+// // coding.forEach((num) => {
+
+// //     if (num > 4) {
+
+// //         hello.push(num);
+
+// //     }
+
+
+// // })
+
+// // console.log(hello);
+
